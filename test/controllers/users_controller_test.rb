d8836904b8094191bbe498417e637fa8b1addbde
LongRuby <thang0908441098@gmail.com>
2024-06-12 11:39:40 +0700

    modify test/ctrler/user_ctrler

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,13 +1,8 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "login with invalid information" do
-    get login_path
-    assert_template 'sessions/new'
-    post login_path, params: { session: { email: "", password: "" } }
-    assert_template 'sessions/new'
-    assert_not flash.empty?
-    get root_path
-    assert flash.empty?
+  test "should get new" do
+    get signup_path
+    assert_response :success
   end
 end
