9dea5541cb02f6eb65644a80e2728bc98f68a78d
LongRuby <thang0908441098@gmail.com>
2024-06-12 11:35:29 +0700

    In process of showing all user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
+  end
+
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
@@ -10,4 +15,33 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+  
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name,
+    email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { name: @user.name,
+    email: @user.email } }
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
 end
